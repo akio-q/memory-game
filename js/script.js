c8bde164fa05398ce1010fda6243820245d72c62
Valentyn <wolf.valentyn@gmail.com>
2022-08-24 16:59:33 +0300

    add matrixGenerator and initializer functions, add some styles

--- js/script.js
+++ js/script.js
@@ -14,18 +14,18 @@ let firstCard = false;
 let secondCard = false;
 
 const items = [
-    { name:'bee', image: '../img/bee.png' },
-    { name: 'crocodile', image: '../img.crocodile.png' },
-    { name: 'macaw', image: '../img.macaw.png' },
-    { name: 'gorilla', image: '../img.gorilla.png' },
-    { name: 'tiger', image: '../img.tiger.png' },
-    { name: 'monkey', image: '../img.monkey.png' },
-    { name: 'chameleon', image: '../img.chameleon.png' },
-    { name: 'piranha', image: '../img.piranha.png' },
-    { name: 'anaconda', image: '../img.anaconda.png' },
-    { name: 'sloth', image: '../img.sloth.png' },
-    { name: 'cockatoo', image: '../img.cockatoo.png' },
-    { name: 'toucan', image: '../img.toucan.png' }
+    { name:'bee', image: 'img/bee.png' },
+    { name: 'crocodile', image: 'img/crocodile.png' },
+    { name: 'macaw', image: 'img/macaw.png' },
+    { name: 'gorilla', image: 'img/gorilla.png' },
+    { name: 'tiger', image: 'img/tiger.png' },
+    { name: 'monkey', image: 'img/monkey.png' },
+    { name: 'chameleon', image: 'img/chameleon.png' },
+    { name: 'piranha', image: 'img/piranha.png' },
+    { name: 'anaconda', image: 'img/anaconda.png' },
+    { name: 'sloth', image: 'img/sloth.png' },
+    { name: 'cockatoo', image: 'img/cockatoo.png' },
+    { name: 'toucan', image: 'img/toucan.png' }
 ];
 
 let seconds = 0,
@@ -67,3 +67,80 @@ const generateRandom = (size = 4) => {
 
     return cardValues;
 };
+
+const matrixGenerator = (cardValues, size = 4) => {
+    gameContainer.innerHTML = '';
+    cardValues = [...cardValues, ...cardValues];
+
+    cardValues.sort(() => Math.random() - 0.5);
+
+    for (let i = 0; i < size * size; i++) {
+        gameContainer.innerHTML += `
+            <div class="card-container" data-card-value="${cardValues[i].name}">
+                <div class="card-before">?</div>
+                <div class="card-after">
+                    <img src="${cardValues[i].image}" class=""image">
+                </div>
+            </div>
+        `;
+    }
+
+    gameContainer.style.gridTemplateColumns = `repeat(${size}, auto)`;
+
+    cards = document.querySelectorAll('.card-container');
+
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            if (!card.classList.contains('matched')) {
+                card.classList.add('flipped');
+
+                if (!firstCard) {
+                    firstCard = card;
+                    firstCardValue = card.getAttribute('data-card-value');
+                } else {
+                    movesCounter();
+    
+                    secondCard = card;
+                    let secondCardValue = card.getAttribute('data-card-value');
+    
+                    if (firstCardValue == secondCardValue) {
+                        firstCard.classList.add('matched');
+                        secondCard.classList.add('matched');
+    
+                        firstCard = false;
+                        winCount += 1;
+    
+                        if (winCount == Math.floor(cardValues.length / 2)) {
+                            result.innerHTML = `
+                                <h2>You Won</h2>
+                                <h4>Moves: ${movesCount}</h4>
+                            `;
+    
+                            stopGame();
+                        }
+                    } else {
+                        let [tempFirst, tempSecond] = [firstCard, secondCard];
+    
+                        firstCard = false;
+                        secondCard = false;
+    
+                        let delay = setTimeout(() => {
+                            tempFirst.classList.remove('flipped');
+                            tempSecond.classList.remove('flipped');
+                        }, 900);
+                    }
+                }
+            } 
+        });
+    });
+};
+
+const initializer = () => {
+    result.innerText = '';
+    winCount = 0;
+
+    let cardValues = generateRandom();
+    matrixGenerator(cardValues);
+};
+
+initializer();
